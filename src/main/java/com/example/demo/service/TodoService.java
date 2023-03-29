d9c8c36f684e8889dcc39cd6e1840538b7d2198d
Hyeon <sophiehan12@naver.com>
2023-03-29 12:59:59 +0900

    3/29일 수업 완료: 백엔드 서버 1차 완료

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @Slf4j
 @Service
@@ -46,5 +48,42 @@ public class TodoService {
 
     public List<TodoEntity> retrieve(final String userId){
         return repository.findByUserId(userId);
+    }
+
+    public List<TodoEntity> update(TodoEntity entity){
+        validate(entity);
+
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+        original.ifPresent(todo ->{
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+            repository.save(todo);
+        });
+
+        return retrieve(entity.getUserId());
+    }
+
+    public List<TodoEntity> delete(final  TodoEntity entity){
+        validate(entity);
+
+        try{
+            repository.delete(entity);
+        }catch (Exception e){
+            log.error("error deleting entity", entity.getId(),e);
+            throw new RuntimeException("error deleting entity" + entity.getId());
+        }
+
+        return retrieve(entity.getUserId());
+    }
+
+    private void validate(final TodoEntity entity){
+        if(entity == null){
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException(("Entity cannot be null."));
+        }if(entity.getUserId() == null){
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException(("Entity cannot be null."));
+        }
     }
 }
